7c26f91f3f65772a5e4c75d5ead7910bda667d2a
msachs77 <sachs.matthew@gmail.com>
2014-10-19 22:29:45 +0200

    problem fix

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
     @pins = Pin.all
   end
@@ -9,7 +10,7 @@ class PinsController < ApplicationController
   def show
   end
 
-    def new
+  def new
     @pin = current_user.pins.build
   end
 
@@ -18,19 +19,19 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.' 
-      else
-        render action: 'new'
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.' 
-      else
-        render action: 'edit'
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
@@ -39,13 +40,17 @@ class PinsController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
     def correct_user
-      @pin = current_user.pins.find(params[:id])
-      redirect_to pins_path if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
     end

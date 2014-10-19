de1c37652d6af14b93256e887147cf860ba55077
msachs77 <sachs.matthew@gmail.com>
2014-10-19 21:43:24 +0200

    add impage upload with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019120758) do
+ActiveRecord::Schema.define(version: 20141019145657) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
